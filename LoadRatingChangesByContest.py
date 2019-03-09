33534bab61f1fa83bbda1c2e129393e54180c04c
Vicfred <vicfredboy@gmail.com>
2019-03-09 10:11:32 -0600

    bulk insert

--- LoadRatingChangesByContest.py
+++ LoadRatingChangesByContest.py
@@ -24,7 +24,9 @@ if __name__ == "__main__":
     DBSession = sessionmaker(bind=engine)
     session = DBSession()
 
+    print("Fetching contest list.")
     contests = codeforces.contest_list(False)
+    print(f"Done. Fetched {len(contests)} contests.")
 
     for contest in contests:
         sleep(1/5)
@@ -32,10 +34,6 @@ if __name__ == "__main__":
         if rating_changes is None:
             continue
         print(f"Processing rating changes for contest {contest.name} got {len(rating_changes)} changes.")
-        for rating_change in rating_changes:
-            try:
-                session.add(RatingChange(**vars(rating_change)))
-                session.commit()
-            except (IntegrityError, InvalidRequestError):
-                pass
-            print(rating_change)
+        rating_changes = [vars(item) for item in rating_changes]
+        session.bulk_insert_mappings(RatingChange, rating_changes)
+        session.commit()
