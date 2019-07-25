cabc0aadde951c8439021a4f0e7af6b40516a1ec
Vicfred <vicfredboy@gmail.com>
2019-07-25 16:05:42 -0500

    comment

--- LoadRatingChangesByContest.py
+++ LoadRatingChangesByContest.py
@@ -1,4 +1,5 @@
 # this script takes about 23.6 minutes to run on all no-gym contests (1135 contests as of 13-Jul-2019 sleep(1/5)
+# this script takes about 21.5 minutes to run on all no-gym contests (1138 contests as of 25-Jul-2019 sleep(1/5)
 import time
 from time import sleep
 
